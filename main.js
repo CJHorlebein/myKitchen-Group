b49cd0c6146e772ef3278c18c9829ce8378fbd84
Robel Birkneh <robika@Robels-MacBook-Pro.local>
2018-11-27 19:29:50 -0500

    commiting main.js.

--- main.js
+++ main.js
@@ -1,3 +1,11 @@
+
+
+
+
+
+
+
+
 var url = `http://api.yummly.com/v1/api/recipes?_app_id=36fb4d31&_app_key=f367d40bf349da1721b89ad0f9c1a622&q=trending&maxResult=8`;
     axios.get(url).then(response => {
         var data = response.data.matches;
