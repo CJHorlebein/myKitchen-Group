b74c55b493c9400e9e91ee9c48588e653e2d6c8a
Robel Birkneh <robika@Robels-MacBook-Pro.local>
2018-11-27 19:44:18 -0500

    added function to make pictures of the recipes a better quality.

--- main.js
+++ main.js
@@ -1,17 +1,15 @@
-
-
-
-
-
-
-
-
+function picQualityCheck(picture){
+    if(picture){
+        return picture[0].slice(0, -4);
+    }
+    return "https://cdn.dribbble.com/users/1012566/screenshots/4187820/topic-2.jpg"
+}
 var url = `http://api.yummly.com/v1/api/recipes?_app_id=36fb4d31&_app_key=f367d40bf349da1721b89ad0f9c1a622&q=trending&maxResult=8`;
     axios.get(url).then(response => {
         var data = response.data.matches;
         recipeCards.innerHTML = "";
         data.forEach(recipe => {
-            var imgURL = recipe.smallImageUrls;
+            var imgURL = picQualityCheck(recipe.smallImageUrls);
             recipeCards.innerHTML += `
             <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3 mt-3">
                 <div class="card h-100 recipe-cards" style="background-color:#EF6461">
