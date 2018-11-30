f06e0da98c2228f9c49780c3132adf5606a93940
Olivia Clay <oliviabclay@gmail.com>
2018-11-29 21:22:29 -0500

    adds nav bar and sign out button

--- dashboard/dashboard.js
+++ dashboard/dashboard.js
@@ -1,3 +1,34 @@
+function renderDashboard() {
+    return `
+    <h1>What's In My Kitchen?</h1>
+ 
+         <input id="input" placeholder="Please add an item to your kitchen!">
+         <h5>*press enter to add item and click the item to delete*</h5>
+         <ul id="list"></ul>`
+}
+ 
+firebase.auth().onAuthStateChanged(function(user) {
+ 
+    if (user) {
+        var dashboardHTML = renderDashboard()
+        document.getElementById('main-container').innerHTML = dashboardHTML;
+    } else {
+        window.location = "http://localhost:3000";
+        alert('Please log in.');
+    }
+});
+ 
+function signOut(){
+    firebase.auth().signOut()
+        .then(function(){
+            alert("You have signed out.");
+            window.location="../";
+        }).catch(function(error) {
+            alert("Something went wrong!");
+        });
+};
+ 
+ 
 function addNewItem() {
     var item = document.getElementById("input").value;
     var ul = document.getElementById("list");
@@ -6,12 +37,17 @@ function addNewItem() {
     ul.appendChild(li);
     document.getElementById("input").value = "";
     li.onclick = removeItem;
-}
+};
+
 document.onkeyup = function(e) {
     if (e.keyCode == 13) {
         addNewItem();
     }
 };
+
 function removeItem(e) {
     e.target.parentElement.removeChild(e.target);
-}
+};
+
+
+
