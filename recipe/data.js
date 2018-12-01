72d7a1ecf83285b7cd9a2f843fdf2a33e5fb5cef
Olivia Clay <oliviabclay@gmail.com>
2018-11-30 19:43:15 -0500

    merge issues

---
+++ recipe/data.js
@@ -0,0 +1,64 @@
+var searchItems = [
+    "meats", 
+    "cuisines", 
+    "diets"
+];
+
+var searchDict = {
+    cuisines: "&allowedCuisine[]=cuisine^cuisine-",
+    meats: "&allowedIngredient[]=",
+    diets: "&allowedDiet[]="
+};
+
+var meats = [
+    "chicken",
+    "pork",
+    "steak"
+];
+
+var pantry = [
+    "eggs",
+    "Virginia Baked Ham",
+    "bbq sauce",
+    "steak",
+    "honey",
+    "milk"
+];
+
+var diets = [
+    "388^Lacto vegetarian",
+    "389^Ovo vegetarian",
+    "390^Pescetarian",
+    "386^Vegan",
+    "387^Lacto-ovo vegetarian",
+    "403^Paleo"
+];
+
+var cuisines = [
+    "american",
+    "kid-friendly",
+    "italian",
+    "asian",
+    "mexican",
+    "southern",
+    "french",
+    "southwestern",
+    "barbecue-bbq",
+    "indian",
+    "chinese",
+    "cajun",
+    "mediterranean",
+    "greek",
+    "english",
+    "spanish",
+    "thai",
+    "german",
+    "moroccan",
+    "irish",
+    "japanese",
+    "cuban",
+    "hawaiian",
+    "swedish",
+    "hungarian",
+    "portuguese"
+];
